5d0f0599a15a5a782a9f1a3d959bbd18ab6736c4
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-15 17:55:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,23 @@
 function convertToRoman(num) {
-  const symbols = [
-    ['M', 1000],
-    ['D', 500],
-    ['C', 100],
-    ['L', 50],
-    ['X', 10],
-    ['V', 5],
-    ['I', 1]
-  ];
+  	const obj = {
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
+	
 
-  let roman = '';
+  //your code here
+	
 
-  for (let i = 0; i < symbols.length; i++) {
-    const [symbol, value] = symbols[i];
-
-    while (num >= value) {
-      roman += symbol;
-      num -= value;
-    }
-
-    // Handle subtractive combinations like IV, IX, etc.
-    if (i % 2 === 0) {
-      const nextIndex = i - (i % 2) + 2;
-      const nextSymbol = symbols[nextIndex][0];
-      const nextValue = symbols[nextIndex][1];
-      if (num >= value - nextValue) {
-        roman += nextSymbol + symbol;
-        num -= (value - nextValue);
-      }
-    }
-  }
-
-  return roman;
 }
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// Test cases
-console.log(convertToRoman(14)); // Output: XIV
-console.log(convertToRoman(798)); // Output: DCCXCVIII
-console.log(convertToRoman(36)); // Output: XXXVI
+// console.log(convertToRoman(36));
+
 
 
 
